7ac79ee9d1fedf1b7e414bb90ae0d9881454a0de
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2026-03-12 23:12:21 -0600

    arrays

---
+++ Javascript/Javascript/src/main/01-Tipos/07-arrays.js
@@ -0,0 +1,11 @@
+let animales = ['perro', 'gato','caballo'];
+console.log(animales);
+console.log(animales[2]);
+
+animales[3] = 'gallina';
+console.log(animales);
+
+const arr = [1,2,3];
+arr.push(4);
+console.log(arr);
+console.log(arr.length);
